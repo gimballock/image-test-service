5ca57495f68b2cf17f0288ee364744c34135a670
Gabriel Reyla <gabreyla@gmail.com>
2015-10-19 17:32:27 -0700

    Fixed autogenerated files

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ source_url 'https://github.datapipe.net/automation/image-test-service'
 issues_url 'https://github.datapipe.net/automation/image-test-service/issues'
 license 'All Rights Reserved'
 description 'This is an image test and build suite that includes x, y and z'
-long_description 'Now I'm going to write a longer description...'
+long_description 'Now I am going to write a longer description...'
 version '0.1.0'
 
 supports 'redhat'
